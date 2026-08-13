9fe10516073cea42e8f5cf01cd280485af6ad769
Tanya Bharti <tanyayadav2552005@gmail.com>
2026-08-14 00:22:26 +0530

    fix: add missing python-dateutil dependency for data seeding

---
+++ test_mongo.py
@@ -0,0 +1,9 @@
+﻿from pymongo import MongoClient
+
+uri = "mongodb+srv://tanyayadav2552005_db_user:z50Lnd2DMdfHYa9B@cluster0.wnjd0ld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+try:
+    client = MongoClient(uri, serverSelectionTimeoutMS=5000)
+    client.admin.command("ping")
+    print("SUCCESS: Connected to Atlas")
+except Exception as e:
+    print("ERROR:", e)
